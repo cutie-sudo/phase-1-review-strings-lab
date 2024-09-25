ec957692d67e9e0f1a76a8cb8e20ba24c7ab7739
Faith nguli <cutie-sudo@gmail.com>
2024-09-25 22:02:33 +0300

    add phase-1-review-strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Faith Nguli';
+console.log(currentUser)
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+console.log(shortGreeting)
